beb721a0dc7007645be66f2c77bb18cc980e9c15
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-17 22:13:28 -0400

    Se limpia proyecto base

--- pages/index/+Page.jsx
+++ pages/index/+Page.jsx
@@ -5,7 +5,7 @@ import { Counter } from './Counter'
 function Page() {
   return (
     <>
-      <h1>Welcome</h1>
+      <h1 className="font-bold">Welcome</h1>
       This page is:
       <ul>
         <li>Rendered to HTML.</li>
--- renderer/Layout.jsx
+++ renderer/Layout.jsx
@@ -3,11 +3,8 @@ export { Layout }
 import React from 'react'
 import PropTypes from 'prop-types'
 import { childrenPropType } from './PropTypeValues'
-import logoUrl from './logo.svg'
 import { PageContextProvider } from './usePageContext'
-import { Link } from './Link'
 import './css/index.css'
-import './Layout.css'
 
 Layout.propTypes = {
   pageContext: PropTypes.any,
@@ -17,55 +14,9 @@ function Layout({ pageContext, children }) {
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
-        <Frame>
-          <Sidebar>
-            <Logo />
-            <Link href="/">Welcome</Link>
-            <Link href="/about">About</Link>
-            <Link href="/star-wars">Data Fetching</Link>
-          </Sidebar>
           <Content>{children}</Content>
-        </Frame>
       </PageContextProvider>
     </React.StrictMode>
-  )
-}
-
-Frame.propTypes = {
-  children: childrenPropType
-}
-function Frame({ children }) {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        maxWidth: 900,
-        margin: 'auto'
-      }}
-    >
-      {children}
-    </div>
-  )
-}
-
-Sidebar.propTypes = {
-  children: childrenPropType
-}
-function Sidebar({ children }) {
-  return (
-    <div
-      id="sidebar"
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        lineHeight: '1.8em',
-        borderRight: '2px solid #eee'
-      }}
-    >
-      {children}
-    </div>
   )
 }
 
@@ -74,32 +25,8 @@ Content.propTypes = {
 }
 function Content({ children }) {
   return (
-    <div id="page-container">
-      <div
-        id="page-content"
-        style={{
-          padding: 20,
-          paddingBottom: 50,
-          minHeight: '100vh'
-        }}
-      >
+    <div>
         {children}
-      </div>
-    </div>
-  )
-}
-
-function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10
-      }}
-    >
-      <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
-      </a>
     </div>
   )
 }
